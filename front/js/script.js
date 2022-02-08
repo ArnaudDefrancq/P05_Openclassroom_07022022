3f90cdf7d5392454482d2d9619892426c9aeb622
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-02-08 12:42:41 +0100

    Ajout du fichier JS pour la page d'accueil et mise en place de l'API

---
+++ front/js/script.js
@@ -0,0 +1,44 @@
+const items = document.getElementById("items");
+const numberOfArticle = 8;
+
+const couchData = () => {
+  fetch(" http://localhost:3000/api/products")
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (value) {
+      console.log(value);
+    })
+    .catch(function (err) {
+      console.log("This is a err", err);
+    });
+};
+
+const createCard = (couch) => {
+  const link = document.createElement("a");
+  items.appendChild(link);
+
+  const article = document.createElement("article");
+  link.appendChild(article);
+
+  const img = document.createElement("img");
+  img.setAttribute("src", couch.imageUrl);
+  img.setAttribute("atl", couch.altTxt);
+  article.appendChild(img);
+
+  const title = document.createElement("h3");
+  title.classList.add("productName");
+  title.innerText = couch.name;
+  article.appendChild(title);
+
+  const description = document.createElement("p");
+  description.classList.add("productDescription");
+  description.innerText = couch.description;
+  article.appendChild(description);
+};
+
+for (let i = 0; i < numberOfArticle; i++) {
+  createCard(i);
+}
