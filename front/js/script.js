8a9968691bc74e7883d416ba6d288db209a7c935
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-02-09 13:06:37 +0100

    correction des différentes fonctions

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,44 @@
 const items = document.getElementById("items");
 const numberOfArticle = 8;
+let array = [];
+
+const couchData = async () => {
+  await fetch("http://localhost:3000/api/products")
+    .then((response) => response.json())
+    .then((data) => (array = data))
+    .catch((err) => console.log("Ceci est une erreur", err));
+};
+
+couchData();
+
+// const createCard = async () => {
+//   await couchData();
+
+//   console.log(array);
+
+//   const link = document.createElement("a");
+//   items.appendChild(link);
+
+//   const article = document.createElement("article");
+//   link.appendChild(article);
+
+//   const img = document.createElement("img");
+//   article.appendChild(img);
+
+//   const title = document.createElement("h3");
+//   title.classList.add("productName");
+//   title.innerText = array.forEach((test) => test.name);
+//   article.appendChild(title);
+
+//   const description = document.createElement("p");
+//   description.classList.add("productDescription");
+//   description.innerText = array[length].description;
+//   article.appendChild(description);
+// };
+
+// for (let i = 0; i < numberOfArticle; i++) {
+//   createCard(i);
+// }
 
 const createCard = () => {
   const link = document.createElement("a");
@@ -20,13 +59,10 @@ const createCard = () => {
   article.appendChild(description);
 };
 
-for (let i = 0; i < numberOfArticle; i++) {
-  createCard(i);
-}
-
-const couchData = () => {
-  fetch(" http://localhost:3000/api/products")
-    .then((response) => response.json())
-    .then((response2) => console.table(response2))
-    .catch((err) => console.log("Ceci est une erreur", err));
+const createAllCard = () => {
+  for (let i = 0; i < numberOfArticle; i++) {
+    createCard(i);
+  }
 };
+
+createAllCard();
